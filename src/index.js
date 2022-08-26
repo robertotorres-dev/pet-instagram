f607f659d0c9f4bb263a010d48ec691152e609aa
cetherdev <robert.hct@gmail.com>
2022-08-25 23:47:11 -0500

    feat: create components category and listofcategories

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { App } from './App'
 
 const container = document.getElementById('app')
 const root = createRoot(container)
 
-root.render(<h1>Hola Platzi</h1>)
+root.render(
+  <App />
+)
