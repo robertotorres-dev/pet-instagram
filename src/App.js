083cb210f53c98b80b62b84498fa936fd3250904
cetherdev <robert.hct@gmail.com>
2022-08-28 18:05:22 -0500

    feat: create photo card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './GlobalStyles'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+
 export const App = () => (
   <>
     <GlobalStyles />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 )
