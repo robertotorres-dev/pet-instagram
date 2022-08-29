34dacc80937958c71c2f5db2a541d9ef021b2668
cetherdev <robert.hct@gmail.com>
2022-08-28 23:17:33 -0500

    feat: using useEffect to change listOfCategories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -4,6 +4,7 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
     fetch('https://petgram-api-cether.vercel.app/categories')
@@ -12,11 +13,30 @@ export const ListOfCategories = () => {
         setCategories(response)
       })
   }, [])
-  return (
-    <List>
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
