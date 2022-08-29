e74fce67a876c084044344a85ef3af62b4553f6a
cetherdev <robert.hct@gmail.com>
2022-08-28 21:54:00 -0500

    feat: create animations in card component

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
-import { categories } from '../../../api/db.json'
+import db from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   return (
     <List>
       {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        db.categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )
