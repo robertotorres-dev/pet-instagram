477683b1199b89cde5bc9e8e846946c2b96a6fea
cetherdev <robert.hct@gmail.com>
2022-08-30 00:04:51 -0500

    feat: create a high order component with query params

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,24 +1,11 @@
 import React from 'react'
-import { useQuery, gql } from '@apollo/client'
 
 // COMPONENTS
 import { PhotoCard } from '../PhotoCard'
+import { useGetPhotos } from '../../hoc/useGetPhotos'
 
-const whitPhotos = gql`
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`
-
-export const ListOfPhotoCards = () => {
-  const { loading, error, data } = useQuery(whitPhotos)
+export const ListOfPhotoCards = ({ categoryId }) => {
+  const { loading, error, data } = useGetPhotos(categoryId)
   if (error) return <h2>Internal Server Error</h2>
   if (loading) return <h2>Loading...</h2>
 
