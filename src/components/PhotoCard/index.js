acea4bbffd45542e0a6e8a24be844f31a7b34ee4
cetherdev <robert.hct@gmail.com>
2022-08-30 21:25:41 -0500

    feat: add mutation in favButton gql

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { ImgWrapper, Img, Article } from './styles'
 import { useNearScreen } from '../../hooks/useNearScreen'
+import { FavButton } from '../FavButton'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useMutationToogleLike } from '../../hoc/useMutationToogleLike'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
+  const { mutation, mutationLoading, mutationError } = useMutationToogleLike()
+  console.log(mutation, mutationLoading, mutationError)
+
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
 
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
+  const handleFavClick = () => {
+    !liked && mutation({
+      variables: { input: { id } }
+    })
+    setLiked(!liked)
+  }
 
   return (
     <Article ref={element}>
@@ -23,9 +32,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size='32px' /> {likes} likes!
-          </Button>
+          <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
         </>
     }
     </Article>
---
+++ src/hoc/useMutationToogleLike.js
@@ -0,0 +1,16 @@
+import { useMutation, gql } from '@apollo/client'
+
+const mutationLikePhoto = gql`
+  mutation likeAnonymousPhoto($input: LikePhoto!){
+    likeAnonymousPhoto(input: $input){
+      id,
+      liked,
+      likes
+    }
+  }
+`
+
+export const useMutationToogleLike = () => {
+  const [mutation, { loading: mutationLoading, error: mutationError }] = useMutation(mutationLikePhoto)
+  return { mutation, mutationLoading, mutationError }
+}
