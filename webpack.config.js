3b0087fda8f1d1ecab7cdfe9c49db0dd50621266
cetherdev <robert.hct@gmail.com>
2022-09-09 12:24:02 -0500

    feat: avoid PWA in our SPA

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
+const path = require('path')
+
 module.exports = {
   mode: 'development',
   output: {
@@ -8,6 +13,49 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      filename: 'manifest.webmanifest',
+      name: 'Pet nstagram - Tu app de fotos de mascotas',
+      short_name: 'Pet-nstragram 🐹',
+      description: 'Con esta app puedes encontrar fotos de animales favoritos',
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: '/',
+      scope: '/',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          purpose: 'any maskable',
+          destination: path.join('Icons'),
+          ios: true
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: 'service-worker.js',
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 5000000,
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://pet-instagram.vercel.app/'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   module: {
